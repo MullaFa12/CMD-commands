4ac085759e4f99f5208861f15cbd99a5ec4deda7
Fatima <fatima@gmail.com>
2024-03-17 23:40:55 +0530

    practice commands

---
+++ src/github/FirstProject.java
@@ -0,0 +1,12 @@
+package github;
+
+public class FirstProject {
+	
+	public static void main (String[]args) {
+		
+		
+		System.out.println("GithubPractice");
+		System.out.println("GithubPractice1");
+	}
+
+}
